970bbbe60e4c50af6e606d98b376aac8854b3506
unknown <ak.vazquez618@gmail.com>
2023-04-23 23:28:35 -0300

    Finished code documentation

--- tarea2/settings/tarea2.Debug.cspy.bat
+++ tarea2/settings/tarea2.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files\IAR Systems\Embedded Workbench 9.1\common\bin\cspybat" -f "C:\Users\devan\OneDrive\Desktop\school\Arqui2\ARQUI-Project2-main\tarea2\settings\tarea2.Debug.general.xcl" --backend -f "C:\Users\devan\OneDrive\Desktop\school\Arqui2\ARQUI-Project2-main\tarea2\settings\tarea2.Debug.driver.xcl" 
+"C:\Program Files\IAR Systems\Embedded Workbench 9.1\common\bin\cspybat" -f "C:\Users\Angel K . Vazquez\Desktop\ARQUI-Project2\tarea2\settings\tarea2.Debug.general.xcl" --backend -f "C:\Users\Angel K . Vazquez\Desktop\ARQUI-Project2\tarea2\settings\tarea2.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files\IAR Systems\Embedded Workbench 9.1\common\bin\cspybat" -f "C:\Users\devan\OneDrive\Desktop\school\Arqui2\ARQUI-Project2-main\tarea2\settings\tarea2.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\devan\OneDrive\Desktop\school\Arqui2\ARQUI-Project2-main\tarea2\settings\tarea2.Debug.driver.xcl" 
+"C:\Program Files\IAR Systems\Embedded Workbench 9.1\common\bin\cspybat" -f "C:\Users\Angel K . Vazquez\Desktop\ARQUI-Project2\tarea2\settings\tarea2.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Angel K . Vazquez\Desktop\ARQUI-Project2\tarea2\settings\tarea2.Debug.driver.xcl" 
 
 @echo off 
 :end
